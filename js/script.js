fb16d7699257047e05b2c50aeb28f9d13a94cd5e
JoshuaTaw <80139403+JoshuaTaw@users.noreply.github.com>
2021-03-26 21:13:55 +0900

    major update

--- js/script.js
+++ js/script.js
@@ -1,116 +1,190 @@
 var pretext, counts, uniqCount;
 
 document.querySelector('#show_output').onclick = function() {
-
-    count();
-
-    text = '';
-    temtext1 = '';
-    temtext2 = '';
-    temtext3 = '';
-
-    if (pretext.slice(-1) !== ',') {
-      pretext += ",";
-    }
-
-    var temtext1 = pretext.replace(/、|，/g, ',');
-
-    var temtext2 = temtext1.replace(/,\s/g, '#3#w#q#');
-
-    var temtext3 = temtext2.replace(/\s/g, ',\n');
-
-    var text = temtext3.replace(/#3#w#q#/g, ',\n');
-
-
-    if (!text.replace(/\s|,/g, '').length) {
-      document.querySelector('#incount').innerText = 0;
-      out0count()
-      return;
-    }
-
-
-    document.querySelector('#incount').innerText = text.replace(/[^","]/g, "").length;
-
-
-    let availableData = text;
-    let desiredData = replaceCommaLine(availableData);
-    function replaceCommaLine(data) {
-        let dataToArray = data.split(',').map(item => item.trim());
-        return dataToArray.join(",\n");
-    }
-    console.log(desiredData);
-
-    var uniqueLines = [];
-
-    var input = desiredData.split('\n');
-
-    for (let line of input) {
-        if (uniqueLines.indexOf(line) === -1) {
-            uniqueLines.push(line)
-        }
-    }
-
-    var uniqueLinesText = '';
-    for (let uniqueLine of uniqueLines) {
-        uniqueLinesText += uniqueLine + '\n';
-    }
-
-//    document.querySelector('#outcount').innerText = uniqueLines.length - 1;
-
-
-    var sorting = uniqueLinesText;
-    if (sorting) {
-      sorting = sorting.split('\n').map(function (item) {
-        return item.trim()
-      });
-      sorting = sorting.sort(function (a, b) {
-        return a.length - b.length
-      }).join('\n');
-    }
-        console.log(sorting);
-
-
-//    if (sorting.slice(3, 4) === ',\n') {
-//    }
-    var sorting = sorting.replace(/^\s+/, "");
-    var sorting = sorting.replace(/^,\s+/, "");
-
-    document.querySelector('#output').value = sorting;
-
-    document.querySelector('#outcount').innerText = sorting.replace(/[^","]/g, "").length;
-
+  if (document.querySelector('#editcount').innerText > 7) {
+    document.getElementById("showalart").style.display = 'block';
+    var eventcounter = 2;
+    document.querySelector('#eventcounter').innerText = eventcounter;
+    return;
+  }
+  showoutput();
 }
 
+function showoutput() {
+  sessionStorage.setItem("autosave", '');
+  count();
+
+  text = '';
+  temtext1 = '';
+  temtext2 = '';
+  temtext3 = '';
+
+  if (pretext.slice(-1) !== ',') {
+    pretext += ",";
+  }
+
+  var temtext1 = pretext.replace(/、|，/g, ',');
+
+  var temtext2 = temtext1.replace(/,\s/g, '#3#w#q#');
+
+  var temtext3 = temtext2.replace(/\s/g, ',\n');
+
+  var text = temtext3.replace(/#3#w#q#/g, ',\n');
+
+
+  if (!text.replace(/\s|,/g, '').length) {
+    document.querySelector('#incount').innerText = 0;
+    recount();
+    return;
+  }
+
+  countclear();
+  document.querySelector('#incount').innerText = text.replace(/[^","]/g, "").length;
+
+
+  let availableData = text;
+  let desiredData = replaceCommaLine(availableData);
+  function replaceCommaLine(data) {
+      let dataToArray = data.split(',').map(item => item.trim());
+      return dataToArray.join(",\n");
+  }
+  console.log(desiredData);
+
+  var uniqueLines = [];
+
+  var input = desiredData.split('\n');
+
+  for (let line of input) {
+      if (uniqueLines.indexOf(line) === -1) {
+          uniqueLines.push(line)
+      }
+  }
+
+  var uniqueLinesText = '';
+  for (let uniqueLine of uniqueLines) {
+      uniqueLinesText += uniqueLine + '\n';
+  }
+
+  var sorting = uniqueLinesText;
+  if (sorting) {
+    sorting = sorting.split('\n').map(function (item) {
+      return item.trim()
+    });
+    sorting = sorting.sort(function (a, b) {
+      return a.length - b.length
+    }).join('\n');
+  }
+      console.log(sorting);
+
+
+  var sorting = sorting.replace(/^\s+/, "");
+  var sorting = sorting.replace(/^,\s+/, "");
+
+  document.querySelector('#output').value = sorting;
+
+  document.querySelector('#outcount').innerText = sorting.replace(/[^","]/g, "").length;
+
+  sessionStorage.setItem("autosave", field.value);
+}
+
+
+// window.onbeforeunload = function() {
+//   if (document.querySelector('#editcount').innerText > 7) {
+//    return "output changed";
+//  }
+// };
 
 
 document.querySelector('#clear').onclick = function() {
-  clear()
+  if (document.querySelector('#editcount').innerText > 7) {
+    document.getElementById("showalart").style.display = 'block';
+    var eventcounter = 1;
+    document.querySelector('#eventcounter').innerText = eventcounter;
+    return;
+  }
+  realclear();
+}
+
+var eventcounter = 0;
+
+document.querySelector('#dismiss').onclick = function() {
+  document.getElementById("showalart").style.display = 'none';
+  document.querySelector('#editcount').innerText = 0;
+
+
+  if (document.querySelector('#eventcounter').innerText == 1) {
+    realclear();
+  }
+
+  if (document.querySelector('#eventcounter').innerText == 2) {
+    showoutput();
+  }
 }
 
 
-function clear() {
+
+function countclear() {
+  innercounter = 0;
+  eventcounter = 0;
+  document.querySelector('#eventcounter').innerText = 0;
+  document.querySelector('#incount').innerText = 0;
+  document.querySelector('#outcount').innerText = 0;
+  document.querySelector('#editcount').innerText = 0;
+}
+
+
+function realclear() {
   pretext = '';
   counts = [];
   uniqCount = [];
+  innercounter = 0;
+  eventcounter = 0;
+  document.querySelector('#eventcounter').innerText = 0;
   document.querySelector('#incount').innerText = 0;
   document.querySelector('#outcount').innerText = 0;
+  document.querySelector('#editcount').innerText = 0;
   document.querySelector('#input').value = '';
   document.querySelector('#output').value = '';
+
+  sessionStorage.setItem("autosave", '');
 }
 
 
-function init() {
+
+let field = document.getElementById("output");
+
+document.querySelector('#output').addEventListener("input", function() {
+  sessionStorage.setItem("autosave", field.value);
+});
+
+
+function refresh() {
   document.querySelector('#incount').innerText = 0;
   document.querySelector('#input').value = '';
-  out0count()
+
+  if (sessionStorage.getItem("autosave")) {
+    // Restore the contents of the text field
+    document.querySelector('#output').value = sessionStorage.getItem("autosave");
   }
+}
 
-window.onload = init;
+window.onload = refresh;
 
 
 
+var innercounter = 0;
 
-// avoid unexpected output overwriting by pressing Enter key
+document.getElementById("output").addEventListener("input",function () {
+  document.querySelector('#editcount').innerText = 0;
+  innercounter += 1;
+  document.querySelector('#editcount').innerText = innercounter;
+
+  outrecount = document.querySelector('#output').value;
+//  var outrecount = outrecount.replace(/、|，/g, ',');
+//  document.querySelector('#output').value = outrecount;
+  document.querySelector('#outcount').innerText = outrecount.replace(/[^","]/g, "").length;
+})
+
 
 // var keyinput = document.getElementById("input");
 //
@@ -122,53 +196,55 @@ window.onload = init;
 // });
 
 
-function out0count() {
-  output0count = [];
-  output0count = document.querySelector('#output').value;
-  document.querySelector('#outcount').innerText = output0count.replace(/[^","]/g, "").length;
+function recount() {
+  document.querySelector('#input').value = document.querySelector('#output').value;
+  document.getElementById("show_output").click();
+
+  pretext = '';
+  counts = [];
+  uniqCount = [];
+  document.querySelector('#incount').innerText = 0;
+  document.querySelector('#input').value = '';
 }
 
 
 
-
 function count() {
-    pretext = '';
-    counts = [];
-    uniqCount = [];
+  pretext = '';
+  counts = [];
+  uniqCount = [];
 
-    document.querySelector('#incount').innerText = 0;
-    document.querySelector('#outcount').innerText = 0;
-    pretext = document.querySelector('#input').value;
+  document.querySelector('#incount').innerText = 0;
+  document.querySelector('#outcount').innerText = 0;
+  pretext = document.querySelector('#input').value;
 
 
+  if (!pretext) return;
 
-    if (!pretext) return;
+  var input = pretext.split('\n');
 
-    var input = pretext.split('\n');
+  if (input.length <= 1) return;
 
-    if (input.length <= 1) return;
+  var dupesFound = false;
 
-    var dupesFound = false;
-
-    for (let line of input) {
-        let dupe = counts.find(count => count.line === line);
-        if (dupe && (line !== '')) {
-            dupe.count++;
-            dupesFound = true;
-        } else {
-            counts.push({
-                line: line,
-                count: 1
-            });
-        }
+  for (let line of input) {
+    let dupe = counts.find(count => count.line === line);
+    if (dupe && (line !== '')) {
+        dupe.count++;
+        dupesFound = true;
+    } else {
+        counts.push({
+            line: line,
+            count: 1
+        });
     }
+  }
 
 
-    if (!dupesFound) return;
+  if (!dupesFound) return;
 
-    counts = counts.filter(function(count) {
-        if (count.count === 1) uniqCount.push(count.line);
-        return count.count !== 1;
-    });
-
+  counts = counts.filter(function(count) {
+    if (count.count === 1) uniqCount.push(count.line);
+    return count.count !== 1;
+  });
 }
